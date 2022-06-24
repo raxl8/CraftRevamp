8beae7708c717e10d6a389d7aa0fd708f9975dcb
darkshoz <binksdark@proton.me>
2022-06-24 23:25:15 +0200

    fix(Player): missing changes for UUID

--- src/Game/Player.h
+++ src/Game/Player.h
@@ -3,6 +3,7 @@
 #include "Networking/TCPStream.h"
 #include "Game/Protocol/Protocol.h"
 #include "Game/Protocol/PacketStream.h"
+#include "Game/UUID.h"
 
 class GameServer;
 class Player
@@ -15,9 +16,13 @@ public:
 	inline void SetUsername(std::string&& username) { m_Username = std::move(username); }
 	inline const std::string& GetUsername() { return m_Username; }
 
+	inline void SetUUID(UUID&& uuid) { m_UUID = std::move(uuid); }
+	inline const UUID& GetUUID() { return m_UUID; }
+
 private:
 	GameServer* m_gameServer;
 	std::weak_ptr<TCPStream> m_TCPStream;
 	std::unique_ptr<Protocol> m_Protocol;
 	std::string m_Username;
+	UUID m_UUID;
 };
